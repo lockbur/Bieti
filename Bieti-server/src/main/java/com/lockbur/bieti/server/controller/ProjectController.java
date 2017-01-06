eefd418ad2f4baba31ecb2aa27a92bc6b6ed6ac8
wangkun23 <wang.kun@jinyinwu.com>
2017-01-06 16:01:04 +0800

    ADD# 增加css和js

---
+++ Bieti-server/src/main/java/com/lockbur/bieti/server/controller/ProjectController.java
@@ -0,0 +1,36 @@
+package com.lockbur.bieti.server.controller;
+
+
+import com.lockbur.bieti.server.domain.Project;
+import com.lockbur.bieti.server.mapper.ProjectMapper;
+import org.jgroups.JChannel;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Administrator
+ */
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+    @Resource
+    private ProjectMapper projectMapper;
+
+
+    @RequestMapping("/edit/{id}")
+    public String displayEdit(@PathVariable("id") Integer id) {
+
+        return "project/edit";
+    }
+
+    @RequestMapping("/update")
+    public String update(Project project) {
+
+        return "/project/edit";
+    }
+}

d872c3eeb92e4b1f8c7fd8d6ddcd0eb3c88bc8ca
wangkun23 <wang.kun@jinyinwu.com>
2017-01-16 16:24:32 +0800

    添加指令规则

--- Bieti-agent/src/main/java/com/lockbur/bieti/agent/command/CommandManager.java
+++ Bieti-agent/src/main/java/com/lockbur/bieti/agent/command/CommandManager.java
@@ -1,20 +1,67 @@
 package com.lockbur.bieti.agent.command;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lockbur.bieti.agent.jgroups.CollectingLogOutputStream;
+import com.lockbur.bieti.common.AgentInstance;
+import com.lockbur.bieti.common.CommandMessage;
+import org.apache.commons.exec.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by wangkun23 on 2017/1/15.
  */
-@Component
 public class CommandManager {
 
     private final Logger logger = LoggerFactory.getLogger(CommandManager.class);
 
     public void processCommand(WebSocketSession webSocketSession, WebSocketMessage<?> webSocketMessage) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            CommandMessage message = mapper.readValue(webSocketMessage.getPayload().toString(), CommandMessage.class);
+
+            for (AgentInstance instance : message.getInstances()) {
+                logger.info("getInstances {}", instance);
+                try {
+                    File dir = new File(instance.getPath() + "/bin");
+
+                    //http://blog.csdn.net/fd_mas/article/details/50147701
+                    DefaultExecutor executor = new DefaultExecutor();
+                    executor.setWorkingDirectory(dir);
+                    ExecuteWatchdog watchdog = new ExecuteWatchdog(60000);
+                    executor.setWatchdog(watchdog);
+
+                    //设置执行命令成功的退出值为1
+                    //executor.setExitValue(1);
+
+                    DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
+
+                    String command = "cmd.exe /C startup.bat";
+                    CommandLine commandline = CommandLine.parse(command);
+
+                    PumpStreamHandler streamHandler = new PumpStreamHandler(new CollectingLogOutputStream());
+
+                    executor.setStreamHandler(streamHandler);
+                    executor.execute(commandline, resultHandler);
+
+                    logger.info("commandline execute success .");
+                    resultHandler.waitFor();
+
+                    logger.info("resultHandler waitFor {}", resultHandler.getException());
+
+                } catch (Exception e) {
+                    logger.error("commandline execute failed {}.", e);
+                }
+            }//for
+        } catch (IOException e) {
+            logger.error("processCommand {}",e);
+        }
         logger.info("{},command {}", webSocketSession.getId(), webSocketMessage.getPayload());
     }
 }
--- Bieti-agent/src/main/java/com/lockbur/bieti/agent/websocket/AgentWebSocketHandler.java
+++ Bieti-agent/src/main/java/com/lockbur/bieti/agent/websocket/AgentWebSocketHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import javax.annotation.Resource;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 
@@ -25,8 +26,6 @@ public class AgentWebSocketHandler implements WebSocketHandler {
 
     private final Logger logger = LoggerFactory.getLogger(AgentWebSocketHandler.class);
 
-    @Autowired
-    private CommandManager commandManager;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession webSocketSession) throws Exception {
@@ -36,44 +35,8 @@ public class AgentWebSocketHandler implements WebSocketHandler {
     @Override
     public void handleMessage(WebSocketSession webSocketSession, WebSocketMessage<?> webSocketMessage) throws Exception {
         logger.info("webSocketMessageClient {}", webSocketMessage.getPayload());
-
-
-        ObjectMapper mapper = new ObjectMapper();
-        CommandMessage message = mapper.readValue(webSocketMessage.getPayload().toString(), CommandMessage.class);
-        //commandManager.processCommand(webSocketSession, webSocketMessage);
-
-        //http://blog.csdn.net/fd_mas/article/details/50147701
-        for(AgentInstance instance:message.getInstances()){
-            logger.info("getInstances {}", instance);
-            try {
-                File dir = new File(instance.getPath()+"/bin");
-
-                DefaultExecutor executor = new DefaultExecutor();
-                executor.setWorkingDirectory(dir);
-                ExecuteWatchdog watchdog = new ExecuteWatchdog(60000);
-                executor.setWatchdog(watchdog);
-
-                //设置执行命令成功的退出值为1
-                executor.setExitValue(1);
-
-                DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
-
-                String command = "cmd.exe /C startup.bat";
-                CommandLine commandline = CommandLine.parse(command);
-
-                PumpStreamHandler streamHandler = new PumpStreamHandler(new CollectingLogOutputStream());
-
-                executor.setStreamHandler(streamHandler);
-                executor.execute(commandline, resultHandler);
-
-                logger.info("commandline execute success .");
-                resultHandler.waitFor();
-
-                logger.info("commandli{}" ,resultHandler.getException());
-            } catch (Exception e) {
-                logger.error("commandline execute failed {}.", e);
-            }
-        }//for
+        CommandManager commandManager = new CommandManager();
+        commandManager.processCommand(webSocketSession, webSocketMessage);
     }
 
     @Override
